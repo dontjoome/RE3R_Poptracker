b1878a04978ca4e418cd51bdd7c14df11579cdfb
Solidus Snake <63137482+dontjoome@users.noreply.github.com>
2024-10-01 13:09:46 -0400

    Delete RE3R-poptracker directory
    Start from scratch

--- RE3R-poptracker/scripts/logic.lua
+++
@@ -1,9 +0,0 @@
-function has(item, amount)
-    local count = Tracker:ProviderCountForCode(item)
-    amount = tonumber(amount)
-    if not amount then
-      return count > 0
-    else
-      return count == amount
-    end
-  end
